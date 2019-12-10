5a8c9489b012d4926ae4738fa3105419b63bbecc
YordanIH <yordan@hashicorp.com>
2019-12-10 16:56:48 +0200

    count code example

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
+
 provider "null" {
 }
 
 resource "null_resource" "example" {
- count=2
+  count = 2
 
- provisioner local-exec {
-	command="echo \"Hello World${count.index}\""
- }
+  provisioner local-exec {
+    command = "echo \"Hello World${count.index}\""
+  }
 }
